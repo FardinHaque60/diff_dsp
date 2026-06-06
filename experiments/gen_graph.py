31fb5b3f8951a5e455347bbc8605b094a53d13f2
Fardin Haque <fardinhaque60@gmail.com>
2026-06-06 13:55:37 -0700

    misc

--- experiments/gen_graph.py
+++ experiments/gen_graph.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-def gen_plots(weights, loss_history):
+def gen_plots(weights, loss_history, title="Learned Filter Weights"):
     plt.figure(figsize=(12, 6))
 
     plt.subplot(2, 1, 1)
-    plt.title("Loss over Time")
+    plt.title("loss over time")
     plt.plot(loss_history, color='red')
 
     plt.subplot(2, 1, 2)
-    plt.title("Learned Filter Weights (The FIR Kernel)")
+    plt.title(title)
     plt.plot(weights, marker='o')
 
     plt.tight_layout()
